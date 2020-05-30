7a38e1d1819a51d606d5cce26c835ac24c6c02c2
Anton Pogodin <anton.pogodin68@bk.ru>
2020-05-30 19:23:16 +0300

    Add project files.

---
+++ ITMO.Cours2020.WindowsForms.Lab5.Ex.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using ExcelObj = Microsoft.Office.Interop.Excel;
+
+namespace ITMO.Cours2020.WindowsForms.Lab5.Ex._1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
